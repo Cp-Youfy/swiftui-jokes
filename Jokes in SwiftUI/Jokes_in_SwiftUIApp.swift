0b50059c306dc8be2fcc7530a963f547fa341b88
cpyoufy <cpyoufy@gmail.com>
2021-06-01 22:37:39 +0800

    Initial Commit

---
+++ Jokes in SwiftUI/Jokes_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_in_SwiftUIApp.swift
+//  Jokes in SwiftUI
+//
+//  Created by rgs on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Jokes in SwiftUITests/Jokes_in_SwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  Jokes_in_SwiftUITests.swift
+//  Jokes in SwiftUITests
+//
+//  Created by rgs on 1/6/21.
+//
+
+import XCTest
+@testable import Jokes_in_SwiftUI
+
+class Jokes_in_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
